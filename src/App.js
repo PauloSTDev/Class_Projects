4bce2ff751216ab1a93e2dbfdafbd995a4ae2e71
SadoGitHubb <afonso.mea@gmail.com>
2020-11-28 19:00:31 -0300

    Linkagem do firebase e verificacao de email e senha

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 
-
-import React from 'react'
+import firebase from './services/FirebaseConnect'
+import React, {useState} from 'react'
 import {
   Button,
   Grid,
-  rgbToHex,
   TextField
-
 } from '@material-ui/core';
+
+
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const login =()=> {
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+
+      .then((retorno) => {
+        console.log(retorno.user.uid)
+      })
+      .catch(
+        (erro) => {
+          console.log(erro)
+      }
+      )
+  }
 
   return (
     <>
@@ -24,23 +43,35 @@ function App() {
               <TextField
               label="E-mail"
               variant="outlined"
-              type="email"/>
+              type="email"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}/>
             </Grid>
             <Grid item sm={12} xs={12} align="center">
               <TextField
               label="Senha"
               variant="outlined"
-              type="password" style={{marginBottom: 5}}/>
+              type="password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              style={{marginBottom: 5}}
+              />
             </Grid>
             <Grid item sm={12} xs={12} align="center">
-            <Button variant="contained"color="primary">
+            <Button
+            onClick={login}
+            variant="contained"
+            color="primary">
               Logar
             </Button>
             </Grid>
           </Grid>
-        <div id="layout">
+          <footer>
+            <div id="layout">
           @Projeto Agro GP. Todos os direitos reservados
         </div>
+          </footer>
+        
         </header>
       </>
 );
